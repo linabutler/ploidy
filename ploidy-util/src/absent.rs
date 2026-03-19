3e2767d265b7939399152b11e20d7fcb3377102c
Lina Butler <lina@yakshaving.ninja>
2026-03-19 14:58:50 -0700

    feat(util): Add more combinators to `AbsentOr`.

--- ploidy-util/src/absent.rs
+++ ploidy-util/src/absent.rs
@@ -1,4 +1,4 @@
-use std::marker::PhantomData;
+use std::{marker::PhantomData, ops::Deref};
 
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
@@ -13,21 +13,31 @@ pub enum AbsentOr<T> {
 }
 
 impl<T> AbsentOr<T> {
+    /// Returns `true` if the value is [`Absent`](Self::Absent).
     #[inline]
     pub fn is_absent(&self) -> bool {
         matches!(self, Self::Absent)
     }
 
+    /// Returns `true` if the value is [`Null`](Self::Null).
     #[inline]
     pub fn is_null(&self) -> bool {
         matches!(self, Self::Null)
     }
 
+    /// Returns `true` if the value is [`Present`](Self::Present).
     #[inline]
     pub fn is_present(&self) -> bool {
         matches!(self, Self::Present(_))
     }
 
+    /// Converts this [`AbsentOr`] into a [`Result`], mapping
+    /// [`Present`] to [`Ok`], and both [`Absent`] and
+    /// [`Null`] to [`AbsentError`].
+    ///
+    /// [`Present`]: Self::Present
+    /// [`Absent`]: Self::Absent
+    /// [`Null`]: Self::Null
     #[inline]
     pub fn ok(self) -> Result<T, AbsentError> {
         match self {
@@ -37,6 +47,7 @@ impl<T> AbsentOr<T> {
         }
     }
 
+    /// Converts from `&AbsentOr<T>` to `AbsentOr<&T>`.
     #[inline]
     pub fn as_ref(&self) -> AbsentOr<&T> {
         match self {
@@ -46,11 +57,125 @@ impl<T> AbsentOr<T> {
         }
     }
 
+    /// Applies `f` to the contained value if [`Present`],
+    /// leaving [`Absent`] and [`Null`] untouched.
+    ///
+    /// [`Present`]: Self::Present
+    /// [`Absent`]: Self::Absent
+    /// [`Null`]: Self::Null
+    #[inline]
+    pub fn map<U>(self, f: impl FnOnce(T) -> U) -> AbsentOr<U> {
+        match self {
+            Self::Absent => AbsentOr::Absent,
+            Self::Null => AbsentOr::Null,
+            Self::Present(value) => AbsentOr::Present(f(value)),
+        }
+    }
+
+    /// Returns `other` if `self` is [`Present`], or propagates
+    /// [`Absent`] and [`Null`].
+    ///
+    /// [`Present`]: Self::Present
+    /// [`Absent`]: Self::Absent
+    /// [`Null`]: Self::Null
+    #[inline]
+    pub fn and<U>(self, other: AbsentOr<U>) -> AbsentOr<U> {
+        match self {
+            Self::Absent => AbsentOr::Absent,
+            Self::Null => AbsentOr::Null,
+            Self::Present(_) => other,
+        }
+    }
+
+    /// Returns the result of applying `f` to the contained value
+    /// if [`Present`], or propagates [`Absent`] and [`Null`].
+    ///
+    /// [`Present`]: Self::Present
+    /// [`Absent`]: Self::Absent
+    /// [`Null`]: Self::Null
+    #[inline]
+    pub fn and_then<U>(self, f: impl FnOnce(T) -> AbsentOr<U>) -> AbsentOr<U> {
+        match self {
+            Self::Absent => AbsentOr::Absent,
+            Self::Null => AbsentOr::Null,
+            Self::Present(value) => f(value),
+        }
+    }
+
+    /// Returns `self` if [`Present`](Self::Present), or `other`
+    /// otherwise.
+    #[inline]
+    pub fn or(self, other: AbsentOr<T>) -> AbsentOr<T> {
+        match self {
+            Self::Present(_) => self,
+            Self::Absent | Self::Null => other,
+        }
+    }
+
+    /// Returns `self` if [`Present`](Self::Present), or computes
+    /// a fallback from `f` otherwise.
+    #[inline]
+    pub fn or_else(self, f: impl FnOnce() -> AbsentOr<T>) -> AbsentOr<T> {
+        match self {
+            Self::Present(_) => self,
+            Self::Absent | Self::Null => f(),
+        }
+    }
+
+    /// Returns the contained value if [`Present`](Self::Present),
+    /// or the provided `default` otherwise.
+    #[inline]
+    pub fn unwrap_or(self, default: T) -> T {
+        match self {
+            Self::Absent | Self::Null => default,
+            Self::Present(value) => value,
+        }
+    }
+
+    /// Returns the contained value if [`Present`](Self::Present),
+    /// or computes a default from `f` otherwise.
+    #[inline]
+    pub fn unwrap_or_else(self, f: impl FnOnce() -> T) -> T {
+        match self {
+            Self::Absent | Self::Null => f(),
+            Self::Present(value) => value,
+        }
+    }
+
+    /// Converts this [`AbsentOr`] into an [`Option`],
+    /// collapsing [`Absent`] and [`Null`] into [`None`].
+    ///
+    /// [`Absent`]: Self::Absent
+    /// [`Null`]: Self::Null
     #[inline]
     pub fn into_option(self) -> Option<T> {
         match self {
             Self::Absent | Self::Null => None,
             Self::Present(value) => Some(value),
+        }
+    }
+}
+
+impl<T: Deref> AbsentOr<T> {
+    /// Converts from `AbsentOr<T>` to `AbsentOr<&T::Target>`.
+    #[inline]
+    pub fn as_deref(&self) -> AbsentOr<&T::Target> {
+        match self {
+            Self::Absent => AbsentOr::Absent,
+            Self::Null => AbsentOr::Null,
+            Self::Present(value) => AbsentOr::Present(value),
+        }
+    }
+}
+
+impl<T: Default> AbsentOr<T> {
+    /// Returns the contained value if [`Present`](Self::Present),
+    /// or the default value of `T` otherwise.
+    #[inline]
+    pub fn unwrap_or_default(self) -> T {
+        match self {
+            Self::Absent | Self::Null => T::default(),
+            Self::Present(value) => value,
         }
     }
 }
@@ -109,6 +234,9 @@ pub enum AbsentError {
 }
 
 impl AbsentError {
+    /// Attaches a field name to this [`AbsentError`], producing a
+    /// [`FieldAbsentError`] suitable for user-facing diagnostics
+    /// when a specific field isn't [`Present`](AbsentOr::Present).
     #[inline]
     pub fn field(self, name: &'static str) -> FieldAbsentError {
         match self {
